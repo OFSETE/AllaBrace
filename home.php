5a716e16800cbc4afc5b0664d6a8619dda4ae9ed
Matheus F. Alves Marinho <93099230+OFSETE@users.noreply.github.com>
2023-04-25 08:43:02 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Minha página PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="logo-container">
+      <img src="imgs/logoAL.png" alt="Logo Alla Brace">
+    </div>
+    <a href="login.php"><button type="submit" class="butao">Entrar</button></a>
+  </body>
+</html>
